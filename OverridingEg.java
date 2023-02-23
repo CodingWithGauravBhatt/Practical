833538aa4a9874e7afe6f6f3c293a1dfc45bf687
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-02-23 17:59:13 +0530

    Add files via upload

---
+++ OverridingEg.java
@@ -0,0 +1,35 @@
+package Today23Feb;
+class Exam{
+	int marks () {
+		return 0;
+	}
+}
+class comp extends Exam{
+	int marks () {
+		return 80;
+	}
+}
+class math extends Exam{
+	int marks () {
+		return 70;
+	}
+}
+class science extends Exam{
+	int marks () {
+		return 50;
+	}
+}
+class eng extends Exam{
+	int marks () {
+		return 90;
+	}
+}
+public class OverridingEg {
+public static void main(String[] args) {
+	Exam exam;
+	exam = new science();
+	System.out.println(exam.marks());
+	exam = new eng();
+	System.out.println(exam.marks());
+}
+}
