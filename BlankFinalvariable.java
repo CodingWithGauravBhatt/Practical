8f40ded76939a280194bdcabefbc5eb8f6e4e1a3
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-02-24 22:38:47 +0530

    Add files via upload

---
+++ BlankFinalvariable.java
@@ -0,0 +1,13 @@
+package Today24Feb;
+
+public class BlankFinalvariable {
+	final int value; //blank initialliztion of final variable
+	BlankFinalvariable(){ //default Contructor
+		value = 70;
+		System.out.println(value);
+//we can't change the value of final variable
+		// compile time error
+	}
+public static void main(String[] args) {
+	BlankFinalvariable fv = new BlankFinalvariable ();	//create object
+}}
