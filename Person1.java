a6f5e2037fdf85892fce7248babcbcd682bb92cd
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-03-01 18:38:35 +0530

    Add files via upload

---
+++ Person1.java
@@ -0,0 +1,11 @@
+package MyPackage;
+
+public class Person1 {
+		private String name;
+		private int age;
+		public Person1 (String name, int age){
+			this.name=name;
+			this.age=age;
+			System.out.println(name+" "+age);
+	}
+}
