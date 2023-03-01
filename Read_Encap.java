828812cd08600cebe51b400231b272da6047750d
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-03-01 18:36:57 +0530

    Add files via upload

---
+++ Read_Encap.java
@@ -0,0 +1,12 @@
+package MyPackage;
+
+public class Read_Encap {
+private String name = "Gaurav";
+public String getName() {
+	return name;
+}
+public static void main(String[] args) {
+	Read_Encap obj = new Read_Encap();
+	System.out.println(obj.name);
+}
+}
