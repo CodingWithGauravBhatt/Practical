84c6fa651b6fd8e497fdd53d98280e1737a03c6f
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-03-02 17:54:01 +0530

    Add files via upload

---
+++ ArrayEg.java
@@ -0,0 +1,15 @@
+package Today2Mar;
+
+public class ArrayEg {
+public static void main(String[] args) {
+	String [] names = {"Mamta","Rani","Laxmi","Heera","Ajay","Tushar"};
+    for (int i =0;i<names.length;i++){
+    System.out.println(names[i]);	}
+    
+	System.out.println();
+	int [] phone = {123,456,567,789,955,333,555,111,246};
+    for (int j = 0;j<phone.length;j++) {
+    	System.out.println(phone[j]);
+    }
+}
+}
