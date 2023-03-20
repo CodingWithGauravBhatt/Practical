5c66b07559f3dd7bc565a03afcb506fa6d976898
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-03-20 18:49:17 +0530

    Add files via upload

--- Assignment.java
+++ Assignment.java
@@ -1,22 +1,49 @@
-class Assignment{
-public static void main(String...args){
-
-int a = 50;
-
-a+= 3;
-
-System.out.println(a);
-
-a-= 10;
-
-System.out.println(a);
-
-a*= 2;
-
-System.out.println(a);
-
-a/= 2;
-
-System.out.println(a);
-}
+package Today20Mar;
+public class Assignment {
+	void display() {
+		try {
+		int i = 1/0;
+		System.out.println(i);
+		}
+		catch (Exception e) {
+		System.out.println("Invalid Devision");	
+		}}
+	void show() {
+		try {
+			String a= "India Is Best";
+			int n= Integer.parseInt(a);
+			System.out.println(n);
+		}
+			catch (Exception e) {
+			System.out.println("Format Mismatch");	
+			}}
+	void print() {
+		try {
+			int cal = 1+50/0;
+			System.out.println(cal);
+			}
+			catch (Exception e) {
+			System.out.println("Calculation Invalid");	
+			}}
+	void run() {
+		try {
+			int arr [] = new int[5];
+			arr[7]=10;
+			}
+			catch (Exception e) {
+			System.out.println("Array index is Invalid");	
+			}}
+	void s() {
+		System.out.println();
+		String s ="Exception handling completed";
+		System.out.println(s);	
+	}
+	public static void main(String[] args) {
+		Assignment ea =new Assignment();
+		ea.display();
+		ea.show();
+		ea.print();
+		ea.run();
+		ea.s();
+	}
 }
