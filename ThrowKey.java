aa9f647c49cb37738bc19b56f5fc9b5096bf73f8
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-03-22 16:59:50 +0530

    Add files via upload

---
+++ ThrowKey.java
@@ -0,0 +1,13 @@
+package Today22Mar;
+
+public class ThrowKey {
+	public static void check(int marks) {
+		if (marks<60) {
+			throw new ArithmeticException("Student is not eligible");
+			}
+		else {
+			System.out.println("Student is eligible for course");
+		}}
+	public static void main(String[] args) {
+		check(58);
+	}}
