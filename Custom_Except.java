09db258fadee5d79760a96fe8084ac2c74aee705
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-03-23 18:23:54 +0530

    Add files via upload

---
+++ Custom_Except.java
@@ -0,0 +1,22 @@
+package Today23Mar;
+
+class  InvalidAgeException extends Exception{ //constructor
+	public InvalidAgeException(String str) { //call the constructor of parent exception
+		super(str);
+	}
+}
+public class Custom_Except {
+	static void checkage(int age ) throws InvalidAgeException{
+if (age<25) {
+	throw new InvalidAgeException("Not eligible for java course");
+}
+else {
+	System.out.println("java course registred sucessfully");
+}}
+	public static void main(String[] args) {
+		try {
+			checkage(23);
+	}
+		catch (InvalidAgeException e) {
+			System.out.println(e);
+}}}
