6771c2a905dae3891a816a2b4827e2cd0e443791
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-04-04 20:51:58 +0530

    Add files via upload

---
+++ AllCharsFIS.java
@@ -0,0 +1,16 @@
+package Today4Mar;
+import java.io.FileInputStream;
+//we can read all character- using FileInputStream
+class AllCharsFIS {
+	public static void main(String[] args) {
+		try {
+	FileInputStream f = new FileInputStream("D:\\java4.txt");
+	int i = 0;
+	while ((i=f.read())!=-1) {
+		System.out.println((char)i);
+	}
+	f.close();
+		}
+	catch (Exception e) {
+		System.out.println(e);
+	}}}
