2850d46ec271966625b650b009d5403d92ebd962
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-04-05 22:29:02 +0530

    Add files via upload

---
+++ FileWriterExample.java
@@ -0,0 +1,23 @@
+package Today5Apr;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class FileWriterExample {
+	public static void main(String[] args) {
+		try {
+			FileWriter fw=new FileWriter(" ");
+			fw.write("Welcome to the java world");
+			FileReader fr=new FileReader("");
+			int i=0;
+			while((i=fr.read())!=-1) {
+				System.out.println((char)i);
+			}
+			//fw.close();
+			fr.close();
+			System.out.println("done");
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
